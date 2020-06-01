5b6220f5677796bf8897e71d3b4d030d9fb5648d
Hyung Kim <hjkmines@gmail.com>
2020-06-01 21:52:42 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -5,12 +5,9 @@ require 'pry'
 
 def snake_it_up(string)
   if string[0] == "s"
-<<<<<<< HEAD
     ("s" * 10) + string
-=======
     10 * ("s".to_i) + string
     binding.pry
->>>>>>> d93a9ffe8b0f32c01a76ee931c056b93f196af88
   else
     string
   end
